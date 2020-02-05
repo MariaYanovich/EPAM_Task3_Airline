4e5ec95721f9910b55751672591428e8deacd71b
MariaYanovich <maryanovichs@gmail.com>
2020-02-05 16:29:15 +0300

    "Replace Main.java to /test/java. Not for always. Main.java will be removed"

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,3 @@
-package by.training.task3.airline;
-
-import by.training.task3.airline.repository.impl.AirlineRepositoryImpl;
-import by.training.task3.airline.service.AirlineService;
 import by.training.task3.airline.service.impl.AirlineServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,14 +6,7 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-//        AirlineRepositoryImpl repository = new AirlineRepositoryImpl();
-//        repository.initializeRepository();
-//        repository.removePlane(new TransportPlane());
-//        System.out.println(repository.getPlaneById(2));
-//        //System.out.println(repository);
-
         AirlineServiceImpl airlineServiceImplementation = new AirlineServiceImpl();
         airlineServiceImplementation.sortByPracticalFlightDistanceAndTypeOfPlane();
-
     }
 }
